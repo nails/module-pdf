fa3bd18f0e15afe32d26124bf05fa13603e7c98b
Pablo de la Peña <p@shedcollective.org>
2017-08-14 11:54:56 +0100

    Updated DomPDF to 0.8.0 and updated wrapper to fit

--- src/Library/Pdf.php
+++ src/Library/Pdf.php
@@ -12,21 +12,71 @@
 
 namespace Nails\Pdf\Library;
 
-use DOMPDF;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Nails\Common\Traits\Caching;
+use Nails\Common\Traits\ErrorHandling;
 use Nails\Factory;
 use Nails\Pdf\Exception\PdfException;
 
 class Pdf
 {
-    use \Nails\Common\Traits\ErrorHandling;
-    use \Nails\Common\Traits\Caching;
+    use ErrorHandling;
+    use Caching;
 
     // --------------------------------------------------------------------------
 
-    private   $oCi;
-    private   $oDomPdf;
-    private   $sDefaultFilename;
-    protected $iInstantiateCount;
+    /**
+     * The default name to give documents
+     */
+    const DEFAULT_FILE_NAME = 'document.pdf';
+
+    /**
+     * The default options, loaded on construction
+     */
+    const DEFAULT_OPTIONS = [
+        'defaultPaperSize'     => 'A4',
+        'isRemoteEnabled'      => true,
+        'fontDir'              => DEPLOY_CACHE_DIR . 'dompdf/font/dir/',
+        'fontCache'            => DEPLOY_CACHE_DIR . 'dompdf/font/cache/',
+        'isHtml5ParserEnabled' => true,
+    ];
+
+    /**
+     * The default paper size
+     */
+    const DEFAULT_PAPER_SIZE = 'A4';
+
+    /**
+     * The default paper orientaton
+     */
+    const DEFAULT_PAPER_ORIENTATION = 'landscape';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Holds a reference to DomPDF
+     * @var Dompdf
+     */
+    protected $oDomPdf;
+
+    /**
+     * Stores any custom options as applied by the user so that they are maintained across instances
+     * @var array
+     */
+    protected $aCustomOptions = [];
+
+    /**
+     * Stores the current paper size
+     * @var string
+     */
+    protected $sPaperSize = '';
+
+    /**
+     * Stores the current paper orientation
+     * @var string
+     */
+    protected $sPaperOrientation = '';
 
     // --------------------------------------------------------------------------
 
@@ -35,217 +85,181 @@ class Pdf
      */
     public function __construct()
     {
-        $this->oCi               =& get_instance();
-        $this->sDefaultFilename  = 'document.pdf';
-        $this->iInstantiateCount = 0;
+        $this->instantiate();
     }
 
     // --------------------------------------------------------------------------
 
     /**
-     * Defines a DOMPDF constant, if not already defined
-     * @param  string $sConstant The name of the constant
-     * @param  string $mValue    The value to give the constant
-     * @return boolean
+     * Instantiate a new instance of DomPDF
+     * @return $this
      */
-    static function setConfig($sConstant, $mValue)
+    protected function instantiate()
     {
-        if (!defined($sConstant)) {
+        $oOptions = new Options();
+        $aOptions = [];
 
-            define($sConstant, $mValue);
-            return true;
-
-        } else {
-
-            return false;
+        //  Default options
+        foreach (self::DEFAULT_OPTIONS as $sOption => $mValue) {
+            $aOptions[$sOption] = $mValue;
         }
+
+        //  Custom options override default options
+        foreach ($this->aCustomOptions as $sOption => $mValue) {
+            $aOptions[$sOption] = $mValue;
+        }
+
+        //  Set the options
+        foreach ($aOptions as $sOption => $mValue) {
+            $oOptions->set($sOption, $mValue);
+        }
+
+        $this->oDomPdf = new Dompdf($oOptions);
+        $this->setPaperSize($this->sPaperSize, $this->sPaperOrientation);
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Unset the instance of DomPDF and re-instantiate
+     * @return $this
+     */
+    public function reset()
+    {
+        unset($this->oDomPdf);
+        $this->instantiate();
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Defines a DomPDF constant, if not already defined
+     *
+     * @param  string $sOption The name of the option
+     * @param  string $mValue  The value to give the option
+     *
+     * @return $this;
+     */
+    public function setOption($sOption, $mValue)
+    {
+        $this->aCustomOptions[$sOption] = $mValue;
+        $this->oDomPdf->set_option($sOption, $mValue);
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Alias to setOption()
+     *
+     * @param  string $sOption The name of the option
+     * @param  string $mValue  The value to give the option
+     *
+     * @return $this
+     * @deprecated
+     */
+    public function setConfig($sOption, $mValue)
+    {
+        return $this->setOption($sOption, $mValue);
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Alias for setting the paper size
+     *
      * @param  string $sSize        The paper size to use
      * @param  string $sOrientation The paper orientation
-     * @return void
+     *
+     * @return $this
      */
-    public function setPaperSize($sSize = 'A4', $sOrientation = 'portrait')
+    public function setPaperSize($sSize = null, $sOrientation = null)
     {
-        if ($this->iInstantiateCount == 0) {
-
-            $this->instantiate();
-        }
-
-        $this->oDomPdf->set_paper($sSize, $sOrientation);
+        $this->sPaperSize        = $sSize ?: static::DEFAULT_PAPER_SIZE;
+        $this->sPaperOrientation = $sOrientation ?: static::DEFAULT_PAPER_ORIENTATION;
+        $this->oDomPdf->setPaper($sSize, $sOrientation);
+        return $this;
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Alias for enabling or disabling remote resources in PDFs
+     *
      * @param  boolean $bValue whether to enable remote resources
-     * @return boolean
+     *
+     * @return $this
      */
-    static function enableRemote($bValue = true)
+    public function enableRemote($bValue = true)
     {
-        return self::setConfig('DOMPDF_ENABLE_REMOTE', $bValue);
+        $this->setOption('isRemoteEnabled', $bValue);
+        return $this;
     }
 
     // --------------------------------------------------------------------------
 
     /**
-     * Instantiates a new instance of DOMPDF
-     * @return void
-     */
-    protected function instantiate()
-    {
-        if ($this->iInstantiateCount == 0) {
-
-            //  Load and configure DOMPDF
-            self::setConfig('DOMPDF_ENABLE_AUTOLOAD', false);
-            self::setConfig('DOMPDF_DEFAULT_PAPER_SIZE', 'A4');
-            self::setConfig('DOMPDF_ENABLE_REMOTE', true);
-            self::setConfig('DOMPDF_FONT_DIR', DEPLOY_CACHE_DIR . 'dompdf/font/dir/');
-            self::setConfig('DOMPDF_FONT_CACHE', DEPLOY_CACHE_DIR . 'dompdf/font/cache/');
-
-            // --------------------------------------------------------------------------
-
-            /**
-             * Test the cache dirs exists and are writable.
-             * ============================================
-             * If the caches aren't there or writable log this as an error,
-             * no need to halt execution as the cache *might* not be used. If
-             * on a production environment errors will be muted and shouldn't affect
-             * anything; but make a not in the logs regardless
-             */
-
-            if (!is_dir(DOMPDF_FONT_DIR)) {
-
-                //  Not a directory, attempt to create
-                if (!@mkdir(DOMPDF_FONT_DIR, 0777, true)) {
-
-                    //  Couldn't create. Sad Panda
-                    log_message(
-                        'error',
-                        'DOMPDF\'s cache folder doesn\'t exist, and I couldn\'t create it: ' . DOMPDF_FONT_DIR
-                    );
-                }
-
-            } elseif (!is_really_writable(DOMPDF_FONT_DIR)) {
-
-                //  Couldn't write. Sadder Panda
-                log_message(
-                    'error',
-                    'DOMPDF\'s cache folder exists, but I couldn\'t write to it: ' . DOMPDF_FONT_DIR
-                );
-            }
-
-            if (!is_dir(DOMPDF_FONT_CACHE)) {
-
-                //  Not a directory, attempt to create
-                if (!@mkdir(DOMPDF_FONT_CACHE, 0777, true)) {
-
-                    //  Couldn't create. Sad Panda
-                    log_message(
-                        'error',
-                        'DOMPDF\'s cache folder doesn\'t exist, and I couldn\'t create it: ' . DOMPDF_FONT_CACHE
-                    );
-                }
-
-            } elseif (!is_really_writable(DOMPDF_FONT_CACHE)) {
-
-                //  Couldn't write. Sadder Panda
-                log_message(
-                    'error',
-                    'DOMPDF\'s cache folder exists, but I couldn\'t write to it: ' . DOMPDF_FONT_CACHE
-                );
-            }
-
-            require_once FCPATH . '/vendor/dompdf/dompdf/dompdf_config.inc.php';
-        }
-
-        $this->oDomPdf = new DOMPDF();
-        $this->iInstantiateCount++;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Loads CI views and passes it as HTML to DOMPDF
+     * Loads CI views and passes it as HTML to DomPDF
+     *
      * @param  mixed $mViews An array of views, or a single view as a string
      * @param  array $aData  View variables to pass to the view
+     *
      * @return void
      */
-    public function loadView($mViews, $aData = array())
+    public function loadView($mViews, $aData = [])
     {
         $sHtml  = '';
-        $aViews = (array) $mViews;
-        $aViews = array_filter($aViews);
+        $aViews = array_filter((array) $mViews);
+        $oView  = Factory::service('View');
 
         foreach ($aViews as $sView) {
-
-            $sHtml .= $this->oCi->load->view($sView, $aData, true);
+            $sHtml .= $oView->load($sView, $aData, true);
         }
 
-        if ($this->iInstantiateCount == 0) {
-            $this->instantiate();
-        }
-
-        $this->oDomPdf->load_html($sHtml);
+        $this->oDomPdf->loadHtml($sHtml);
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Renders the PDF and sends it to the browser as a download.
+     *
      * @param  string $sFilename The filename to give the PDF
-     * @param  array  $aOptions  An array of options to pass to DOMPDF's stream() method
-     * @return void
+     * @param  array  $aOptions  An array of options to pass to DomPDF's stream() method
+     *
+     * @return void|bool
      */
-    public function download($sFilename = '', $aOptions = array())
+    public function download($sFilename = '', $aOptions = [])
     {
-        $sFilename = $sFilename ? $sFilename : $this->sDefaultFilename;
+        $sFilename = $sFilename ? $sFilename : self::DEFAULT_FILE_NAME;
 
         //  Set the content attachment, by default send to the browser
         $aOptions['Attachment'] = 1;
 
-        if ($this->iInstantiateCount == 0) {
-
-            $this->instantiate();
-        }
-
-        try {
-
-            $this->oDomPdf->render();
-            $this->oDomPdf->stream($sFilename, $aOptions);
-            exit();
-
-        } catch(\Exception $e) {
-
-            $this->setError($e->getMessage());
-            return false;
-        }
+        $this->oDomPdf->render();
+        $this->oDomPdf->stream($sFilename, $aOptions);
+        exit();
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Renders the PDF and sends it to the browser as an inline PDF.
+     *
      * @param  string $sFilename The filename to give the PDF
-     * @param  array  $aOptions  An array of options to pass to DOMPDF's stream() method
+     * @param  array  $aOptions  An array of options to pass to DomPDF's stream() method
+     *
      * @return void
      */
-    public function stream($sFilename = '', $aOptions = array())
+    public function stream($sFilename = '', $aOptions = [])
     {
-        $sFilename = $sFilename ? $sFilename : $this->sDefaultFilename;
+        $sFilename = $sFilename ? $sFilename : self::DEFAULT_FILE_NAME;
 
         //  Set the content attachment, by default send to the browser
         $aOptions['Attachment'] = 0;
-
-        if ($this->iInstantiateCount == 0) {
-
-            $this->instantiate();
-        }
 
         $this->oDomPdf->render();
         $this->oDomPdf->stream($sFilename, $aOptions);
@@ -256,35 +270,38 @@ class Pdf
 
     /**
      * Saves a PDF to disk
+     *
      * @param  string $sPath     The path to save to
      * @param  string $sFilename The filename to give the PDF
+     *
      * @return boolean
      */
     public function save($sPath, $sFilename)
     {
         if (!is_writable($sPath)) {
-
             $this->setError('Cannot write to ' . $sPath);
             return false;
         }
 
-        if ($this->iInstantiateCount == 0) {
-
-            $this->instantiate();
-        }
-
-        $sPath = rtrim($sPath, '/') . '/';
-
         $this->oDomPdf->render();
-        return (bool) file_put_contents($sPath . $sFilename, $this->oDomPdf->output());
+        $bResult = (bool) file_put_contents(
+            rtrim($sPath, '/') . '/' . $sFilename,
+            $this->oDomPdf->output()
+        );
+
+        $this->reset();
+
+        return $bResult;
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Saves the PDF to the CDN
+     *
      * @param  string $sFilename The filename to give the object
      * @param  string $sBucket   The name of the bucket to upload to
+     *
      * @return mixed             stdClass on success, false on failure
      */
     public function saveToCdn($sFilename, $sBucket = null)
@@ -292,23 +309,24 @@ class Pdf
         if (isModuleEnabled('nailsapp/module-cdn')) {
 
             //  Save temporary file
+            $sCacheDir  = DEPLOY_CACHE_DIR;
             $sCacheFile = 'TEMP-PDF-' . md5(uniqid() . microtime(true)) . '.pdf';
-            if ($this->save(DEPLOY_CACHE_DIR, $sCacheFile)) {
+            if ($this->save($sCacheDir, $sCacheFile)) {
 
-                $oCdn = Factory::service('Cdn', 'nailsapp/module-cdn');
+                $oCdn    = Factory::service('Cdn', 'nailsapp/module-cdn');
                 $oResult = $oCdn->objectCreate(
-                    DEPLOY_CACHE_DIR . $sCacheFile,
+                    $sCacheDir . $sCacheFile,
                     $sBucket,
-                    array('filename_display' => $sFilename)
+                    ['filename_display' => $sFilename]
                 );
 
                 if ($oResult) {
 
-                    @unlink(DEPLOY_CACHE_DIR . $sCacheFile);
+                    @unlink($sCacheDir . $sCacheFile);
+                    $this->reset();
                     return $oResult;
 
                 } else {
-
                     $this->setError($oCdn->lastError());
                     return false;
                 }
@@ -318,7 +336,6 @@ class Pdf
             }
 
         } else {
-
             $this->setError('CDN module is not available');
             return false;
         }
@@ -327,31 +344,19 @@ class Pdf
     // --------------------------------------------------------------------------
 
     /**
-     * Unsets the instance of DOMPDF and reinstanciates
-     * @return void
-     */
-    public function reset()
-    {
-        unset($this->oDomPdf);
-        $this->instantiate();
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * MagicMethod routes any method calls to this class to DOMPDF if it exists
+     * MagicMethod routes any method calls to this class to DomPDF if it exists
+     *
      * @param  string $sMethod    The method called
      * @param  array  $aArguments Any arguments passed
+     *
      * @return mixed
+     * @throws PdfException
      */
-    public function __call($sMethod, $aArguments = array())
+    public function __call($sMethod, $aArguments = [])
     {
         if (method_exists($this->oDomPdf, $sMethod)) {
-
-            return call_user_func_array(array($this->oDomPdf, $sMethod), $aArguments);
-
+            return call_user_func_array([$this->oDomPdf, $sMethod], $aArguments);
         } else {
-
             throw new PdfException('Call to undefined method Pdf::' . $sMethod . '()');
         }
     }
