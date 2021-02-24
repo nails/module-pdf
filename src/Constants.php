97d88a1b956d9f37c2022b6747822c32615b8b31
Pablo de la Peña <p@shedcollective.org>
2021-02-24 17:32:29 +0000

    Add  module Constants class

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nails\Pdf;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\Pdf
+ */
+class Constants
+{
+    /**
+     * The slug for this module
+     *
+     * @var string
+     */
+    const MODULE_SLUG = 'nails/module-pdf';
+}
